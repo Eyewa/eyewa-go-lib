a989e877659383c81e9a49ebc69f46633342db89
uozgen <ugur.ozgen@eyewa.com>
2021-06-26 19:01:03 +0300

    CATALOG-17 - Add context parameter to sync instruments

--- metrics/instrument.go
+++ metrics/instrument.go
@@ -1,6 +1,7 @@
 package metrics
 
 import (
+	"context"
 	"go.opentelemetry.io/otel/attribute"
 )
 
@@ -10,10 +11,20 @@ func (c Counter) Add(value float64, labels ...attribute.KeyValue) {
 	c.Float64Counter.Add(c.ctx, value, labels...)
 }
 
+// AddWithContext adds the value with given context
+func (c Counter) AddWithContext(ctx context.Context, value float64, labels ...attribute.KeyValue) {
+	c.Float64Counter.Add(ctx, value, labels...)
+}
+
 // Add adds the value to the counter's sum. The labels should contain
 // the keys and values to be associated with this value.
 func (u UpDownCounter) Add(value float64, labels ...attribute.KeyValue) {
 	u.Float64UpDownCounter.Add(u.ctx, value, labels...)
+}
+
+// AddWithContext adds the value with given context
+func (u UpDownCounter) AddWithContext(ctx context.Context, value float64, labels ...attribute.KeyValue) {
+	u.Float64UpDownCounter.Add(ctx, value, labels...)
 }
 
 // Record adds a new value to the list of ValueRecorder's records. The
@@ -21,4 +32,9 @@ func (u UpDownCounter) Add(value float64, labels ...attribute.KeyValue) {
 // this value.
 func (v ValueRecorder) Record(value float64, labels ...attribute.KeyValue) {
 	v.Float64ValueRecorder.Record(v.ctx, value, labels...)
+}
+
+// RecordWithContext adds the value with given context
+func (u UpDownCounter) RecordWithContext(ctx context.Context, value float64, labels ...attribute.KeyValue) {
+	u.Float64UpDownCounter.Add(ctx, value, labels...)
 }
