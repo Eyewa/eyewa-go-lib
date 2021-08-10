dc5c6f52cc309d121029991fcf6ee61b62db530b
Dele <85560474+cyclotiq@users.noreply.github.com>
2021-08-10 22:06:18 +0200

    CATALOG-85: Eyewa Product entity definitions (#16)
    * eyewa entity definitions
    
    * go mod tidy
    
    * Feature/catalog 104 - Tracing doc link (#17)
    
    * added context to resource
    
    * updated tracing readme with confluence doc link
    
    * added rabbitmq tracing doc
    
    * Update tracing/README.md
    
    Co-authored-by: Dele <85560474+cyclotiq@users.noreply.github.com>
    
    Co-authored-by: Dele <85560474+cyclotiq@users.noreply.github.com>
    
    Co-authored-by: Jason Truter <79902798+jason-truter@users.noreply.github.com>

---
+++ base/constants.go
@@ -0,0 +1,11 @@
+package base
+
+// List of EyewaProduct types - based on Magento's
+const (
+	SimpleProduct       EyewaProductType = "simple"
+	ConfigurableProduct EyewaProductType = "configurable"
+	DownloadableProduct EyewaProductType = "downloadable"
+	VirtualProduct      EyewaProductType = "virtual"
+	BundledProduct      EyewaProductType = "bundled"
+	GroupedProduct      EyewaProductType = "grouped"
+)
