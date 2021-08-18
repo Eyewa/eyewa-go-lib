09468f62b87d2d33f004ee68fc37f6879a70763d
Jason Truter <79902798+jason-truter@users.noreply.github.com>
2021-08-18 16:06:47 +0200

    Update base/types.go
    Co-authored-by: Dele <85560474+cyclotiq@users.noreply.github.com>

--- base/types.go
+++ base/types.go
@@ -31,7 +31,7 @@ type MagentoProductEvent struct {
 	StoreCode    string  `json:"store_code"`                 // store code for store eyewa_kwd, eyewa_sasone etc
 	StoreLocale  string  `json:"store_locale"`               // store locale for store sa-sone, kw-ar, sa-en etc
 	CreatedAt    string  `json:"created_at"`                 // time in RFC3339 format of when event ocurred
-	EntityID     int     `json:"entity_id"`                  // ID of the product/category
+	EntityID     int     `json:"entity_id"`                  // ID of the product in magento
 	StoreID      int     `json:"store_id"`                   // ID of the store the product/category belongs to
 	EventSubType string  `json:"event_subtype"`              // product-simple/product-simple-custom/product-configurable", // Would be empty for category events
 	Errors       []Error `json:"errors" binding:"omitempty"` // provides reasons why a message ended up in the deadletter queue for e.g
