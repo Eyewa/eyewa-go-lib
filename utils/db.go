11f6d50d8ffb5dd47fcf9ccab10be3152a268b71
uozgen <ugur.ozgen@eyewa.com>
2021-09-14 13:43:49 +0300

    Add product record util

---
+++ utils/db.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"encoding/json"
+	"math/rand"
+	"time"
+
+	"github.com/eyewa/eyewa-go-lib/base"
+)
+
+func createProductRecord() base.ProductModel {
+	subtype := GenerateEventSubType()
+	product := GenerateProduct(subtype)
+
+	data, err := json.Marshal(product)
+	logError(err)
+
+	return base.ProductModel{
+		ProductMeta: base.ProductMeta{
+			ID:             uint(rand.Int()),
+			StoreID:        1,
+			StoreCode:      "eyewa",
+			EntityID:       rand.Int(),
+			ParentEntityID: rand.Int(),
+		},
+		Data:      data,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
